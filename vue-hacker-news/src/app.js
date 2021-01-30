b476066793185526d6347089204b758332492609
kureharyosuke <ohhayo.com@gmail.com>
2021-01-30 13:53:26 +0900

    #1. vue.config.js

---
+++ vue-hacker-news/src/app.js
@@ -0,0 +1,17 @@
+//1
+var a = 10;
+//2
+var a = 10
+
+if (a === 10) {
+    console.log('10이다');
+}
+
+// if (a ===1) console.log('10이다') b() c()
+
+//# Trailng comma 트레일링 콤마
+
+// components: {
+//     '컴포넌트 이름': 컴포넌트 내용,
+//     '컴포넌트 이름': 컴포넌트 내용
+// }
