952d471ba62b0e4c010aa613e56d0f101393644f
kureharyosuke <ohhayo.com@gmail.com>
2021-01-30 16:59:10 +0900

    #4. routes URL = mode: "history" & routes

--- vue-hacker-news/src/components/ToolBar.vue
+++ vue-hacker-news/src/components/ToolBar.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="header">
       <!-- a tag | to는 URl 입력  -->
-      <router-link to="/news">News</router-link>
-      <router-link to="/ask">Ask</router-link>
-      <router-link to="/jobs">Jobs</router-link>
+      <router-link to="/news">News</router-link> |
+      <router-link to="/ask">Ask</router-link> |
+      <router-link to="/jobs">Jobs</router-link> 
   </div>
 </template>
 
@@ -13,6 +13,28 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+ .header{
+   display: flex;
+   padding: 8px;
+   font-size: 2rem;
+   font-weight: bold;
+   color: #fff;
+   background-color: #42b883;
+ }
+.header .router-link-exact-active {
+  color: #35495e;
+}
+.header a {
+  color: #fff;
+}
 
+ /* .header a:active {
+   router-link-exact-active router-link-active
+ } */
+ /* .header a:active {
+    color: -webkit-link;
+    cursor: pointer;
+    text-decoration: underline; 
+    }*/
 </style>
