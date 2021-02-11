98bf0dd754287fbb2297b1a9235d694e7e500c4e
kureharyosuke <ohhayo.com@gmail.com>
2021-02-11 23:24:53 +0900

    v-html api

--- vue-hacker-news/src/views/ItemView.vue
+++ vue-hacker-news/src/views/ItemView.vue
@@ -1,54 +1,61 @@
 <template>
   <div>
-      <section>
-          <!-- 질문 상세정보 -->
-            <div>
-              <div class="flex items-center" >
-                  <router-link :to="`/user/${fetchedItem.user}`" >
-                      <a-icon type="user" />
-                      {{ fetchedItem.user }}
-                  </router-link>
-                  <div>
-                      {{ fetchedItem.time_ago}}
-                  </div>
-              </div>
-            </div>
-            <h2>{{fetchedItem.title}}</h2>
-            <button class="bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white...">
-                Hover me
-            </button>
-      </section>
-      <section>
-          <!-- 질문 댓글 -->
-      </section>
-      <p>ID : {{fetchedItem.id}}</p>
-      <p>TITLE : {{fetchedItem.title}}</p>
-      <p>POINTS : {{fetchedItem.points}}</p>
-      <p>{{fetchedItem.user }} by {{fetchedItem.time_ago}}</p>
-      <p>POINTS : {{fetchedItem.content}}</p>
+    <section>
+      <!-- 질문 상세정보 -->
+      <div>
+        <div class="flex items-center justify-between text-3xl">
+          <router-link :to="`/user/${fetchedItem.user}`">
+            <!-- <a-icon type="user" />
+            <v-mdi name="mdi-account" />
+            <vue-mdi :icon="['mdi', 'account']" />
+            <mdi-account /> -->
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="text-sm">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2 class="flex items-center text-3xl">
+            <menu-icon />
+            {{ fetchedItem.title }}
+      </h2>
+      <!-- <button
+        class="bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white..."
+      >
+        Hover me
+      </button> -->
+    </section>
+    <section>
+      <!-- 질문 댓글 변환시켜야하기때문에 v-html -->
+      <div v-html="fetchedItem.content">
+        <!-- <p>{{ fetchedItem.content }}</p> -->
+      </div>
+    </section>
+    <p>ID : {{ fetchedItem.id }}</p>
+    <p>TITLE : {{ fetchedItem.title }}</p>
+    <p>POINTS : {{ fetchedItem.points }}</p>
+    <p>{{ fetchedItem.user }} by {{ fetchedItem.time_ago }}</p>
   </div>
 </template>
 
 <script>
-import {  mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
 
-
+import { mdiAccount } from '@mdi/js';
 
 export default {
-
-computed: {
-        ...mapGetters([
-            'fetchedItem'
-        ])
-    },
-    created() {
-        console.log(this.$route.params.id)
-        const itemId = this.$route.params.id;
-        this.$store.dispatch('FETCH_ITEM', itemId)
-    }
-}
+  components: { MenuIcon, mdiAccount },
+  computed: {
+    ...mapGetters(["fetchedItem"]),
+  },
+  created() {
+    console.log(this.$route.params.id);
+    const itemId = this.$route.params.id;
+    this.$store.dispatch("FETCH_ITEM", itemId);
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
